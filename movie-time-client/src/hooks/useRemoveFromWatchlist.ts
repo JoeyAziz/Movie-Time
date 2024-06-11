02c1a46b629ce9826ca4cf23649d06a4089305ab
Youssef Aly <youssef.azizaly@gmail.com>
2024-06-11 03:23:08 +0300

    add notif

--- movie-time-client/src/hooks/useRemoveFromWatchlist.ts
+++ movie-time-client/src/hooks/useRemoveFromWatchlist.ts
@@ -1,12 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { moviesApi } from "../api";
+import { useMessage } from "./useMessage";
 
 export function useRemoveFromWatchlist(movieId: string) {
   const queryClient = useQueryClient();
+  const { addMessage } = useMessage();
 
   return useMutation({
     mutationFn: () => moviesApi.removeFromWatchlist(movieId),
     onSuccess: () => {
+      addMessage("Removed movie from your watch list", "success");
       queryClient.invalidateQueries({ queryKey: ["movie", movieId] });
     },
   });
